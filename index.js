90c2cf2212e9defa68606a02dae15e7e2cf1fa34
UmerVtilte <sharofiddinov.07@mail.ru>
2022-02-19 16:23:31 +0500

    Update index.js

--- index.js
+++ index.js
@@ -25,12 +25,24 @@ bot.command('about', async (ctx) => {
     }
 })
 
-
 bot.command('help', (ctx) => {
     try {
         ctx.reply("Просто отправьте название своего города или деревни, и вы получите прогноз погоды.\n\nЕсли вы столкнулись с какой-либо ошибкой или проблемой, отправьте сообщение @telegraf_blog")
     } catch (e) { }
 });
 
-
+bot.on('message', async (ctx) => {
+    try {
+        let weather = (await axios.get(`${process.env.API}${ctx.message.text}`))
+        
+        if (weather.data.error === null && weather.data.result.length !== 0) {
+            var data = weather.data.result[0]
+            ctx.replyWithHTML(`Сегодняшняя погода в <b><u>${data.location.name}</u></b>\n\n<b>${data.current.skytext}</b>\n\nТекущая температура: <b>${data.current.temperature} °C</b>\nКак будто: <b>${data.current.feelslike} °C</b>\nВлажность : <b>${data.current.humidity} %</b><em>\n\nПринес вам @telegraf_blog</em>`);
+        } else {
+            ctx.reply("Извините, ничего не найдено.\n\nПожалуйста, отправьте название вашего города/деревни еще раз.\nУбедитесь, что в названии вашего города/деревни есть пробел, или попробуйте указать название большого города рядом с вами.\n\nЕсли возникла эта проблема продолжает отправлять сообщение на @telegraf_blog.");
+        }
+    } catch (error) {
+        
+    }
+})
 bot.launch()
